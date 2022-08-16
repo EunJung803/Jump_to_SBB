fb8e1f118a93476d4833ad2a679d4086d6a1b503
EunJung803 <aimee3322@gmail.com>
2022-08-17 03:09:31 +0900

    Refactor : 도메인 별로 패키지 분리

--- src/main/java/com/ll/exam/sbb/answer/AnswerRepository.java
+++ src/main/java/com/ll/exam/sbb/answer/AnswerRepository.java
@@ -1,24 +1,17 @@
-package com.ll.exam.sbb;
+package com.ll.exam.sbb.answer;
 
+import com.ll.exam.sbb.answer.Answer;
 import com.ll.exam.sbb.base.RepositoryUtil;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
-public interface QuestionRepository extends JpaRepository<Question, Integer>, RepositoryUtil {
-
-    Question findBySubject(String s);
-
-    Question findBySubjectAndContent(String s, String s1);
-
-    List<Question> findBySubjectLike(String s);
+public interface AnswerRepository extends JpaRepository<Answer, Integer>, RepositoryUtil {
 
     @Transactional
     @Modifying
-    @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
+    @Query(value = "ALTER TABLE answer AUTO_INCREMENT = 1", nativeQuery = true)
     void truncate();    // 이거 지우면 안됨, truncateTable 하면 자동으로 이게 실행됨
 
     @Transactional
