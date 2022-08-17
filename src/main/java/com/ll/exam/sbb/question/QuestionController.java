324bd919729b9108a210cacf8b3537696222f07f
EunJung803 <aimee3322@gmail.com>
2022-08-18 04:00:09 +0900

    Feat : 한 페이지에서 10개씩 나오도록

--- src/main/java/com/ll/exam/sbb/question/QuestionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -2,6 +2,7 @@ package com.ll.exam.sbb.question;
 
 import com.ll.exam.sbb.answer.AnswerForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -25,12 +26,12 @@ public class QuestionController {
     private final QuestionService questionService;    // final 붙은건 자동적으로 @Autowired 된다.
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Question> questionList = questionService.getList();
+    public String list(Model model, @RequestParam(defaultValue = "0") int page) {
+        Page<Question> paging = questionService.getList(page);
 
         // 미래에 실행된 question_list.html 에서
         // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
-        model.addAttribute("questionList", questionList);
+        model.addAttribute("paging", paging);
 
         return "question_list";
     }
