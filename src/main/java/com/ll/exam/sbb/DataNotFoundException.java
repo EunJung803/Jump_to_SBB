34d776c0f59f85f55ed3c7ba5d531142c41dee3c
EunJung803 <aimee3322@gmail.com>
2022-08-18 00:34:50 +0900

    Comment : RuntimeException에 대한 주석 추가

--- src/main/java/com/ll/exam/sbb/DataNotFoundException.java
+++ src/main/java/com/ll/exam/sbb/DataNotFoundException.java
@@ -1,6 +1,9 @@
 package com.ll.exam.sbb;
 
 public class DataNotFoundException extends RuntimeException {
+    // RuntimeException으로 해야 해당 요청을 바로 중지시켜버릴 수 있다
+    // RuntimeException으로 달지 않으면 Service 부분에서 try catch문이 필수임
+
     public DataNotFoundException(String question_not_found) {
     }
 }
